597e5a05d0559d1f06d4f49a4da685fc0a46cb44
Gustavo Vasconcelos <vasconcelosgu604@gmail.com>
2023-05-05 11:19:05 -0300

    :sparkles: Back End Certificate

--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -1,4 +1,5 @@
 import CertificadoFront from "../public/Certificado Front.jpg";
+import CertificadoBack from "../public/Certificado Back.jpg";
 import Image from "next/image";
 
 export default function Certificates() {
@@ -6,6 +7,17 @@ export default function Certificates() {
     <div className="lg:flex gap-10">
       <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-gray-400 flex-1">
         <Image src={CertificadoFront} alt="aplicação" width={650} height={650} />
+        <button className="bg-blue-500 mt-6 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          <a
+            target="_blank"
+            href="https://www.credential.net/b1d52ba1-5cc3-44a4-959f-f1f1d8fb0127"
+          >
+            Acesse o Certificado
+          </a>
+        </button>
+      </div>
+      <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-gray-400 flex-1">
+        <Image src={CertificadoBack} alt="aplicação" width={650} height={650} />
         <button className="bg-blue-500 mt-6 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           <a
             target="_blank"
